f45c77043362186cacd362a458d194a56a8d110d
dogevip <104245437+dogevip@users.noreply.github.com>
2022-04-24 20:15:27 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,21 @@
 !function(){
+var mtest=document.getElementById("test");
+
+var mhtml="<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=G-G52J1DKGNB'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G52J1DKGNB');
+</script>";
+
+var mdiv1=document.body.createElement("div");
+var mdiv2=document.body.createElement("div");
+mtest.appendChild(mdiv);
+mdiv1.innerHTML=mhtml;
 var curv=1;
 if(version<curv){
-document.getElementById("test").innerHTML="<a href=''><p align='center'>点击更新</p></a>"}}();
+mtest.innerHTML="";
+mdiv2.innerHTML="<a href=''><p align='center'>点击更新</p></a>"}}();
