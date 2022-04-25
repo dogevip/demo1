c9ca482e07b4d05e2bbef3438de6df68dff47628
dogevip <104245437+dogevip@users.noreply.github.com>
2022-04-25 11:45:28 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -2,8 +2,8 @@
 var mtest=document.getElementById("test");
 
 var mhtml="<script async src='https://www.googletagmanager.com/gtag/js?id=G-G52J1DKGNB'></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-G52J1DKGNB');</script>";
-var mdiv1=document.body.createElement("div");
-var mdiv2=document.body.createElement("div");
+var mdiv1=document.createElement("div");
+var mdiv2=document.createElement("div");
 mtest.appendChild(mdiv);
 mdiv1.innerHTML=mhtml;
 var curv=1;
