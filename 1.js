a5ae395d42002cd9eff6e680baf3694e187a5945
dogevip <104245437+dogevip@users.noreply.github.com>
2022-04-25 11:35:46 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,13 +1,7 @@
 !function(){
 var mtest=document.getElementById("test");
 
-var mhtml="<script async src='https://www.googletagmanager.com/gtag/js?id=G-G52J1DKGNB'></script><script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-G52J1DKGNB');
-</script>";
+var mhtml="<script async src='https://www.googletagmanager.com/gtag/js?id=G-G52J1DKGNB'></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-G52J1DKGNB');</script>";
 
 var mdiv1=document.body.createElement("div");
 var mdiv2=document.body.createElement("div");
