0c8d03996fef31f9de7fb96b3c35cb01428f3811
dogevip <104245437+dogevip@users.noreply.github.com>
2022-04-26 18:50:04 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,4 +2,5 @@
 var mtest=document.getElementById("test");
 var curv=1;
 if(version<curv){
-mtest.innerHTML="<a href=''><p align='center'>点击更新</p></a>"}}();
+mtest.innerHTML="<a href=''><p align='center'>点击更新</p></a>"}
+}();
