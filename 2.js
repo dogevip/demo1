146cbd71159386b8fa1b5bf558f2ca0ca9dd2b02
dogevip <104245437+dogevip@users.noreply.github.com>
2022-10-09 16:20:11 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,12 +1,5 @@
-!function(){
-var mtest=document.getElementById("test");
-
-var mhtml="<script async src='https://www.googletagmanager.com/gtag/js?id=G-G52J1DKGNB'></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag('js', new Date());  gtag('config', 'G-G52J1DKGNB');</script>";
-var mdiv1=document.createElement("div");
-var mdiv2=document.createElement("div");
-mtest.appendChild(mdiv);
-mdiv1.innerHTML=mhtml;
-var curv=1;
-if(version<curv){
-mtest.innerHTML="";
-mdiv2.innerHTML="<a href=''><p align='center'>点击更新</p></a>"}}();
+{
+  "update":0,
+    "url":"",
+      "description":""
+}
