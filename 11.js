434c6a4dd2089f69a7dcf40993af4e58e64752ca
dogevip <104245437+dogevip@users.noreply.github.com>
2022-10-22 12:59:46 +0800

    Update 11.js

--- 11.js
+++ 11.js
@@ -50,7 +50,58 @@
         }
       },
       "webname": "txxlcdc"
-    }
+    },
+    
+{
+	"type": 1,
+	"webPlay": {
+		"lineContainerx": "//ul[@class=\"stui-play__list clearfix\"]",
+		"webPlaylines": {
+			"epsodeinfox": "//ul/li/a/text()",
+			"epsodeurlx": "//ul/li/a/@href",
+			"linenamex": "//h3/text()"
+		}
+	},
+	"webSearch": {
+		"apiurl": "https://www.fantuanhd.com/search/wd/{query}.html",
+		"replacement": "{query}"
+	},
+	"webSearchApiMv": {
+		"mvItemContainer": "//div[@class=\"stui-pannel__bd clearfix\"]/ul",
+		"webSearchApiMvItem": {
+			"coverx": "//a[@class=\"stui-vodlist__thumb lazyload\"]/@data-original",
+			"playlisturlx": "//a[@class=\"stui-vodlist__thumb lazyload\"]/@href",
+			"statusx": "//a[@class=\"stui-vodlist__thumb lazyload\"]/span[2]/text()",
+			"titlex": "//a[@class=\"stui-vodlist__thumb lazyload\"]/@title"
+		}
+	},
+	"webname": "fantuan"
+},
+{
+	"type": 1,
+	"webPlay": {
+		"lineContainerx": "//div[@id=\"panel1\"]",
+		"webPlaylines": {
+			"epsodeinfox": "//a/span/text()",
+			"epsodeurlx": "//a/@href",
+			"linenamex": "//h3/text()"
+		}
+	},
+	"webSearch": {
+		"apiurl": "https://dsxys.pro/sb/ke7nhZe3c1-.html?wd={query}",
+		"replacement": "{query}"
+	},
+	"webSearchApiMv": {
+		"mvItemContainer": "//div[@class=\"module-items module-card-items\"]",
+		"webSearchApiMvItem": {
+			"coverx": "//img/@data-original",
+			"playlisturlx": "//div[@class=\"module-card-item module-item\"]/a/@href",
+			"statusx": "//div[@class=\"module-item-note\"]/text()",
+			"titlex": "//img/@alt"
+		}
+	},
+	"webname": "dsxys"
+}
 
 
   ]
